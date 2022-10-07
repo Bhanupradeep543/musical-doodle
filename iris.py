c7d3b9b6f5d0aba6dffcef4bd58ed7416bbdd7ec
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2022-10-07 15:17:38 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -17,7 +17,7 @@ This app predicts the **Iris flower** type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 6.9, 5.4)
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 2.9, 5.4)
     sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
     petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
     petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
