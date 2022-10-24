7fe44880fab7222c567e2e9309f6ec5dcabcc2ae
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2022-10-24 21:59:35 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -24,13 +24,13 @@ st.subheader("Select the Options Below")
 
 # adding upload button for giving test data input to model
 data_file = st.file_uploader("Upload SAP EXCEL file",type=['xlsx'])
-
+data_file.to_csv ("sap.csv", index = None, header=True)
+data=pd.DataFrame(pd.read_csv("sap.csv"))
+st.write(data_file)
+st.write(data)
 if st.button("Process"):
     if data_file is not None:
         file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
-        
-        data_file.to_csv ("sap.csv", index = None, header=True)
-        data=pd.DataFrame(pd.read_csv("sap.csv"))
         st.subheader('total data points')
         st.dataframe(data.shape)
 
