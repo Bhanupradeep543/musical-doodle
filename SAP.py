5a0ab87e43d470e224a5a85849bac3a8fb5ac3e5
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2022-11-07 13:27:49 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -37,6 +37,12 @@ if st.button("Process"):
         st.subheader("User status of notification")
         st.write(data['User status'].value_counts().head())
         st.subheader("Repeated notifications ")
+        st.write("Select the date range for repeated notifications") 
+        d = st.date_input("From", )
+        st.write(d)
+        e = st.date_input("TO", )
+        st.write(e)
+        
         st.write("File consists of TOP 300 notifications with same functional location")
         a=data['System'].value_counts().head(300)
         st.write(a)
