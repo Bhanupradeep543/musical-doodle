b51a76fe93c0148619510f11fb106bbe50efca5a
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2022-11-08 09:09:48 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -25,7 +25,7 @@ d = st.date_input("From", )
 e = st.date_input("TO", )
 st.subheader("Repeated notifications Planner group wise")
 options = st.multiselect('Select the planner Group',['CIA','CIB','CIC','CID','CIN','CIV','CNI','EAP','EBP','EBR','MAP','MBP','MBM','MTM'])
-st.write(options)
+st.write(type(options))
 data_file = st.file_uploader("Upload SAP EXCEL file",type=['xlsx'])
 if st.button("upload"):
     if data_file is not None:
