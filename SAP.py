29c3cbb2cb7161614d30d59f65ad81be7a02efa4
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2023-01-25 08:46:25 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -23,6 +23,10 @@ st.write("""# SEIL SAP Notification Dashboard """) # Tittle addition
 st.subheader("Select the From and To date for notifications") 
 d = st.date_input("From", )
 e = st.date_input("TO", )
+st.subheader("Select the Planner group for obtaining repeated notifications")
+options = st.multiselect('Select the planner Group',['CIA','CIB','CIC','CID','CIN','CIV','CNI','EAP','EBP','EBR','MAP','MBP','MBM','MTM'])
+c=options[0]
+st.write(c)
 data_file = st.file_uploader("Upload SAP EXCEL file",type=['xlsx'])
 if st.button("upload"):
     if data_file is not None:
@@ -31,17 +35,13 @@ if st.button("upload"):
         file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
         st.subheader('Total notifications')
         st.write(data.shape[0])
-        st.subheader("Max. notifications Reported by")
-        st.bar_chart(data['Reported by'].value_counts().head(10))
-        st.subheader("Max. notifications Planner group wise")
-        st.bar_chart(data['Planner group'].value_counts().head(7))
-        st.subheader("User status of notification")
-        st.write(data['User status'].value_counts().head())
+        #st.subheader("Max. notifications Reported by")
+        #st.bar_chart(data['Reported by'].value_counts().head(10))
+        #st.subheader("Max. notifications Planner group wise")
+        #st.bar_chart(data['Planner group'].value_counts().head(7))
+        #st.subheader("User status of notification")
+        #st.write(data['User status'].value_counts().head())
         st.subheader("Repeated notifications Planner group wise")
-        st.subheader("Select the Planner group")
-        options = st.multiselect('Select the planner Group',['CIA','CIB','CIC','CID','CIN','CIV','CNI','EAP','EBP','EBR','MAP','MBP','MBM','MTM'])
-        c=options[0]
-        st.write(c)
         plngrp=pd.DataFrame()
         for i in range(data.shape[0]):
           if c==data['Planner group'][i]:
