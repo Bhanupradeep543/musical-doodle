b3b21820150ee5981ebf032e0c38f1449575d804
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2023-01-29 21:40:18 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -43,9 +43,10 @@ if st.button("upload"):
         #st.write(data['User status'].value_counts().head())
         st.subheader("Repeated notifications Planner group wise")
         plngrp=pd.DataFrame()
-        for i in range(data.shape[0]):
-          st.write(data['Planner group'][i])
         j=0
+        for i in range(data.shape[0]):
+          j+=1
+        sb.write(j)
         for i in range(data.shape[0]):          
           if c==data['Planner group'][i-1]:
             j+=1
